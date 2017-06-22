9ce01dc752d62c62eb8b276698f7cf2d6bc5707f
jakebillings <jake@jakebillings.com>
2017-06-22 08:40:26 -0600

    Add author public key info to Transaction class

--- transaction.py
+++ transaction.py
@@ -2,13 +2,23 @@ import signature
 
 
 class Transaction:
-    def __init__(self, data, signature):
+    def __init__(self, data, author, signature):
         self.data = data
+        self.author = author
         self.signature = signature
 
     def get_string(self):
-        return '%s (%s)' % (self.data, self.signature)
+        return '%s (%s)' % (self.data, self.author)
+
+    def get_data(self):
+        return self.data
+
+    def get_author(self):
+        return self.author
+
+    def get_signature(self):
+        return self.signature
 
 
 def sign_transaction(data, key):
-    return Transaction(data, signature.sign(key, data))
+    return Transaction(data, signature.sign(key, data), signature.key_to_string(key.publickey()))
