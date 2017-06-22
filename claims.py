b2b7c90cb3ce9366f321e3a33d81c186e7b1e045
jakebillings <jake@jakebillings.com>
2017-06-21 22:33:01 -0600

    Experiment with RSA signatures

---
+++ claims.py
@@ -0,0 +1,22 @@
+import os
+from encode import encode,decode
+import Crypto.PublicKey.RSA as RSA
+
+data = 'test signed data'
+
+
+def key_to_string(key):
+    return encode(key.exportKey(format="DER"))
+
+key = RSA.generate(1024, os.urandom)
+
+signature = key.sign(data, '')[0]
+
+print 'private', key_to_string(key)
+print 'public', key_to_string(key.publickey())
+print 'data', data
+print 'signature', encode(signature)
+
+print encode(signature)
+
+print key.verify(data+'asdf',(decode(encode(signature),type='long'),None))
