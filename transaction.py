b9c87376748065d91a8355f141b90aa8d6f505d1
jakebillings <jake@jakebillings.com>
2017-06-26 09:40:32 -0600

    Document transaction.py

--- transaction.py
+++ transaction.py
@@ -1,6 +1,8 @@
 import signature
 
 
+# The Transaction class contains a piece of data, an author address, and a signature.
+# This represents a claim made on the block chain. Transactions will be added to blocks.
 class Transaction:
     def __init__(self, data, author, signature):
         self.data = data
@@ -20,5 +22,9 @@ class Transaction:
         return self.signature
 
 
+# Sign a piece of data as a transaction using a given PyCrypto key
+#
+# data must be a string claim to be made on the blockchain
+# key must be a PyCrypto RSA key
 def sign_transaction(data, key):
-    return Transaction(data, signature.sign(key, data), signature.key_to_string(key.publickey()))
+    return Transaction(data, signature.sign(key, data), signature.public_key_to_address(key.publickey()))
